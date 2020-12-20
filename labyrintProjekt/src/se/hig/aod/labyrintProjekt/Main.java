99f29ba546e857d58be89b155e0d10f0af051b3b
Andreas R <andreas.roghe@gmail.com>
2020-12-20 13:33:40 +0100

    Fixade så att vi har ett main program att köra i...
    Tåg sådan jävla tid med JUnit.

---
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/Main.java
@@ -0,0 +1,13 @@
+package se.hig.aod.labyrintProjekt;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        AldousBorderMazeGenerator generator = new AldousBorderMazeGenerator(20);
+        
+        generator.printMaze();
+        
+    }
+
+}
