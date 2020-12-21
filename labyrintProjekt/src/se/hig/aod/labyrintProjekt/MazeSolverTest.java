f8711d496b3158d805b34efcdfefd7d1b978c161
Andreas R <andreas.roghe@gmail.com>
2020-12-21 12:09:36 +0100

    Ingen ändring.

--- labyrintProjekt/src/se/hig/aod/labyrintProjekt/MazeSolverTest.java
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/MazeSolverTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class MazeSolverTest {
     
     MazeGenerator generator = new MazeGenerator();
-    AldousBorderMazeGenerator aldousGenerator = new AldousBorderMazeGenerator(250);
+    AldousBorderMazeGenerator aldousGenerator = new AldousBorderMazeGenerator(20);
 
     @BeforeEach
     void setUp() throws Exception {
