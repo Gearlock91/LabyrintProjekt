5d98a98d90bf565aebcd4d0be8307c5e1242ea96
sofiiiia <sofiaagren1@hotmail.com>
2020-12-21 11:57:46 +0100

    main och textfil

--- labyrintProjekt/src/se/hig/aod/labyrintProjekt/Main.java
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/Main.java
@@ -1,25 +1,70 @@
 package se.hig.aod.labyrintProjekt;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     
     public static void main(String[] args) {
         
-    	String labyrint ="Labyrint/testLabyrint.txt";
+    	String labyrint ="Labyrint/testLabyrint";
+    	converter(labyrint);
+        MazeSolver ms = new MazeSolver();
+        //ms.solveMaze(in);
+        
+    }
+    private static String[][] converter(String labyrint){
+    	String[][] maze;
     	File file = new File(labyrint);
+    	Scanner inSize = null;
+    	BufferedReader inChar = null;
+		try {
+			inChar = new BufferedReader(new FileReader( file));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     	try {
-			Scanner in = new Scanner(file);
+			inSize = new Scanner(file);
+			
+			System.out.println("Complete");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        AldousBorderMazeGenerator generator = new AldousBorderMazeGenerator(20);
-        
-        generator.printMaze();
-        
+    	
+    	int counter = 0;
+    	while(inSize.hasNextLine()) {
+    		inSize.nextLine();
+    		counter++;
+	
+    	}
+    	maze = new String[counter][counter];
+    	System.out.println("Maze size: " + counter*counter);
+    	int i = 0;
+    
+		while (i <= maze.length) {
+    		for(int j = 0; j <= maze.length; j++) {
+    			try {
+    				char a = (char)inChar.read();
+    				System.out.print(a);
+    			} catch (IOException e) {
+			
+    				e.printStackTrace();
+    			}
+    			//a.concat(inChar.next());
+    			//maze[i][j] = a;
+    		}
+    		i++;	
+    		
+    	}
+    	 
+		return null;
+    	
     }
 
 }
