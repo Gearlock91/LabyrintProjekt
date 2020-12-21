4a96dbcf37879cde1c9430f05fb56cc2a9481757
sofiiiia <sofiaagren1@hotmail.com>
2020-12-21 09:42:42 +0100

    la till textfil och main

--- labyrintProjekt/src/se/hig/aod/labyrintProjekt/Main.java
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/Main.java
@@ -1,9 +1,21 @@
 package se.hig.aod.labyrintProjekt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
         
+    	String labyrint ="Labyrint/testLabyrint.txt";
+    	File file = new File(labyrint);
+    	try {
+			Scanner in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         AldousBorderMazeGenerator generator = new AldousBorderMazeGenerator(20);
         
         generator.printMaze();
