c56ca8a799b57dc0a5bda873997d9d7cce39a598
sofiiiia <sofiaagren1@hotmail.com>
2020-12-22 09:04:11 +0100

    javadoc i maze interface

--- labyrintProjekt/src/se/hig/aod/labyrintProjekt/Maze.java
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/Maze.java
@@ -1,9 +1,28 @@
 package se.hig.aod.labyrintProjekt;
-
+/**
+ * 
+ * @author Sofia Ågren
+ * @author Andreas Roghe
+ *
+ */
 public interface Maze {
-
+	/**
+	 * Skapar yttre väggar och sätter de som fasta.
+	 */
 	void createPerimeter();
+	/**
+	 * Skapar en labyrint.
+	 */
 	void createBoard();
+	/**
+	 * Skapar vägarna i labyrinten.
+	 * @param width.
+	 * @param height.
+	 */	
 	void createMaze(int width, int height);
+	/**
+	 * Hämtar labyrinten och konverterar den.
+	 * @return convertMaze.
+	 */
 	String[][] getMaze();
 }
