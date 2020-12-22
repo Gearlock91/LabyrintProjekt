5148842da1aa11d20340edd36d366118f2581050
sofiiiia <sofiaagren1@hotmail.com>
2020-12-22 09:06:19 +0100

    javadoc i solver interface

--- labyrintProjekt/src/se/hig/aod/labyrintProjekt/Solver.java
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/Solver.java
@@ -1,8 +1,20 @@
 package se.hig.aod.labyrintProjekt;
-
+/**
+ * 
+ * @author Sofia Ågren
+ * @author Andreas Roghe
+ *
+ */
 public interface Solver {
-	
+	/**
+	 * Löser labyrinten.
+	 * @param maze
+	 */
 	void solvMaze(String[][] maze);
+	/**
+	 * Skriver ut labyrinten.
+	 * @param maze
+	 */
 	void printMaze(String[][] maze);
 	
 	
