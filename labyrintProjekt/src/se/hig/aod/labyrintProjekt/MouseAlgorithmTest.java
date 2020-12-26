61a135dd0ea440b744c96d4ffbcff9811a33d29e
Andreas R <andreas.roghe@gmail.com>
2020-12-26 19:39:37 +0100

    Lätt test för algoritmen.

---
+++ labyrintProjekt/src/se/hig/aod/labyrintProjekt/MouseAlgorithmTest.java
@@ -0,0 +1,29 @@
+package se.hig.aod.labyrintProjekt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MouseAlgorithmTest {
+    
+    Solver wf;
+    Maze ab;
+    
+    @BeforeEach
+    void setUp() throws Exception {
+        wf = new MouseAlgorithm();
+        ab = new AldousBorderAlgorithm(10);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    @Test
+    void test() {
+        wf.solveMaze(ab.getMaze());
+    }
+
+}
